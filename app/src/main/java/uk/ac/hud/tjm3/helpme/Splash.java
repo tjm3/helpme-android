1fab12e51758e56d2b785e5fb7e73b3ecc1b7463
tmkn8 <tmkn8@protonmail.ch>
2016-02-26 14:29:38 +0000

    Add Splash redirection to login

--- app/src/main/java/uk/ac/hud/tjm3/helpme/Splash.java
+++ app/src/main/java/uk/ac/hud/tjm3/helpme/Splash.java
@@ -1,13 +1,28 @@
 package uk.ac.hud.tjm3.helpme;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Splash extends AppCompatActivity {
+    final static int SPLASH_SCREEN_DELAY = 4000;
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        this.handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        }, Splash.SPLASH_SCREEN_DELAY);
     }
 }
