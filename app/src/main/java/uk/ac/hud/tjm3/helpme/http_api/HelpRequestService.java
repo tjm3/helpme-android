0860054c7ea28f3a8112adbc2f33baba7a121ece
Tomasz Knapik <tmkn0808@gmail.com>
2016-03-11 12:31:07 +0000

    Add basic Help Request service interface

---
+++ app/src/main/java/uk/ac/hud/tjm3/helpme/http_api/HelpRequestService.java
@@ -0,0 +1,42 @@
+package uk.ac.hud.tjm3.helpme.http_api;
+
+import retrofit2.Call;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+import uk.ac.hud.tjm3.helpme.HelpRequest;
+import uk.ac.hud.tjm3.helpme.HelpRequestList;
+
+public interface HelpRequestService {
+    @GET("/help-requests")
+    Call<HelpRequestList> getHelpRequestList();
+
+    @GET("/help-requests")
+    Call<HelpRequestList> getHelpRequestList(
+            @Query("longitude") float longitude,
+            @Query("latitude") float latitude);
+
+    @GET("/help-requests")
+    Call<HelpRequestList> getHelpRequestList(
+            @Query("longitude") float longitude,
+            @Query("latitude") float latitude,
+            @Query("radius") int radius);
+
+    @POST("/help-requests")
+    Call<HelpRequestList> postHelpRequest();
+
+    @GET("/help-requests/{id}")
+    Call<HelpRequest> getHelpRequest(
+            @Path("id") int id);
+
+    @DELETE("/help-requests/{id}")
+    Call<HelpRequest> deleteHelpRequest(
+            @Path("id") int id);
+
+    @PUT("/help-requests/{id}")
+    Call<HelpRequest> updateHelpRequest(
+            @Path("id") int id);
+}
