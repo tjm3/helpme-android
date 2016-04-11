0cf5f9e407f0e230a9040f809e0bab29f9f3182d
Tomasz Knapik <tmkn0808@gmail.com>
2016-04-11 15:40:13 +0100

    Add logining in

--- app/src/main/java/uk/ac/hud/tjm3/helpme/LoginActivity.java
+++ app/src/main/java/uk/ac/hud/tjm3/helpme/LoginActivity.java
@@ -4,7 +4,11 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import uk.ac.hud.tjm3.helpme.exceptions.InvalidLoginCredentialsRuntimeException;
 import uk.ac.hud.tjm3.helpme.http_api.UserSession;
 
 public class LoginActivity extends AppCompatActivity {
@@ -12,6 +16,9 @@ public class LoginActivity extends AppCompatActivity {
     private UserSession userSession;
     public static final String TEST_LOGIN = "testaccount";
     public static final String TEST_PASSWORD = "testtest123";
+    private Button loginButton;
+    private EditText loginEditText, passwordEditText;
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,20 +27,45 @@ public class LoginActivity extends AppCompatActivity {
 
         this.userSession = UserSession.getInstance();
 
-        // Sign in with test credentials, just for now
-        this.userSession.signIn(TEST_LOGIN, TEST_PASSWORD);
+        this.loginButton = (Button) findViewById(R.id.buttonLogin);
+        this.loginEditText = (EditText) findViewById(R.id.txtNewUsername);
+        this.passwordEditText = (EditText) findViewById(R.id.txtNewPassword);
 
-        // TODO(tmkn8): Implement logging in later
-
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
+        this.loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                Intent intent = new Intent(LoginActivity.this, HelpRequestListActivity.class);
-                startActivity(intent);
-                finish();
+            public void onClick(View v) {
+                LoginActivity.this.loginButton.setClickable(false);
+                String password = LoginActivity.this.passwordEditText.getText().toString();
+                String login = LoginActivity.this.loginEditText.getText().toString();
+
+                try {
+                    LoginActivity.this.userSession.signIn(login, password);
+                } catch(InvalidLoginCredentialsRuntimeException e) {
+                    e.printStackTrace();
+                    LoginActivity.this.handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            LoginActivity.this.loginButton.setClickable(true);
+
+                        }
+                    }, 3000);
+                    return;
+
+                }
+
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent(LoginActivity.this, HelpRequestListActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }, 1000);
             }
-        }, 1000);
+        });
+
+
 
     }
 }
